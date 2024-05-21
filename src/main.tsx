320816bce032b59477edd07ac76f703ce15a3b49
Chathura Prasanga <chathuraprasanga98@gmail.com>
2024-05-21 17:40:30 +0530

    second

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
     <App />
-  </Provider>
 );
--- src/redux/actions/index.ts
+++
@@ -1,1 +0,0 @@
-export { getAllEmployees } from "./employeeAction"
